78d6ff76f0b85022c5db506f5b9354a2f8661838
Tung <93968563+tungtd34511@users.noreply.github.com>
2023-05-28 18:24:18 +0700

    Add project files.

---
+++ LearnMVCwithFigma/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LearnMVCwithFigma.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
